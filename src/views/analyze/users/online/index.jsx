146aa39c84fc13b5be44212354ec7d7ff50b9e54
ganxunzou <ganxunzou@163.com>
2018-09-07 11:36:47 +0800

    add

--- src/views/analyze/users/online/index.jsx
+++ src/views/analyze/users/online/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Collapse } from "antd";
+import { Collapse, Row, Col } from "antd";
 const Panel = Collapse.Panel;
 import HistoryArea from "./historyArea";
 import OnlinePie from "./onlinePie";
@@ -8,18 +8,37 @@ class UserOnline extends Component {
 	render() {
 		return (
 			<div
-				style={{
-					overflow: "hidden"
-				}}
 			>
-				<Collapse bordered={false} defaultActiveKey={["1", "2"]}>
-					<Panel header={<div style={{fontSize: '16px', color: '#939393'}}>用户活跃度</div>} key="1">
-						<HistoryArea />
-					</Panel>
-					<Panel header={<div style={{fontSize: '16px', color: '#939393'}}>用户在线人数</div>} key="2">
-						<OnlinePie />
-					</Panel>
-				</Collapse>
+				<Row>
+					<Col xl={{ span: 12 }} >
+						<Collapse bordered={false} defaultActiveKey={["1"]}>
+							<Panel style={{height: '500px'}}
+								header={
+									<div style={{ fontSize: "16px", color: "#939393" }}>
+										用户活跃度
+									</div>
+								}
+								key="1"
+							>
+								<HistoryArea />
+							</Panel>
+						</Collapse>
+					</Col>
+					<Col xl={{ span: 12 }} >
+						<Collapse bordered={false} defaultActiveKey={["2"]}>
+							<Panel style={{height: '500px'}}
+								header={
+									<div style={{ fontSize: "16px", color: "#939393" }}>
+										用户在线人数
+									</div>
+								}
+								key="2"
+							>
+								<OnlinePie />
+							</Panel>
+						</Collapse>
+					</Col>
+				</Row>
 			</div>
 		);
 	}
